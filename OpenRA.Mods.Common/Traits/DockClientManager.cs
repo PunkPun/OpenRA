78022f26e34d5ad724c1eb76dc768295741be49a
Gustas <gustas.kazukauskas@gmail.com>
2024-02-19 12:11:10 +0200

    Add support for not displaying target lines

--- OpenRA.Mods.Common/Traits/DockClientManager.cs
+++ OpenRA.Mods.Common/Traits/DockClientManager.cs
@@ -182,7 +182,8 @@ namespace OpenRA.Mods.Common.Traits
 					target.Actor,
 					null,
 					order.OrderString == "ForceLink",
-					true));
+					true,
+					DockLineColor));
 
 				self.ShowTargetLines();
 			}
